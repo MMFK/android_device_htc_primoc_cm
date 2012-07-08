d349333bc89535b72c6f95dee612cc30e4127863
Jmz <jmztaylor@gmail.com>
2012-07-08 13:24:22 -0400

    testing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2011 mdj
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,74 +24,85 @@
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
-
-# inherit from common msm7x30
--include device/htc/msm7x30-common/BoardConfigCommon.mk
 
 TARGET_SPECIFIC_HEADER_PATH := device/htc/primoc/include
 
-# inherit from the proprietary version
--include vendor/htc/primoc/BoardConfigVendor.mk
-
 TARGET_BOOTLOADER_BOARD_NAME := primoc
 
-#GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := primoc
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 836763136
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 1023409664
-BOARD_FLASH_BLOCK_SIZE := 4096
-
-# TARGET_SPECIFIC_HEADER_PATH := device/htc/primoc/include
-TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
-
-BOARD_UMS_LUNFILE := /sys/devices/platform/msm_otg/msm_hsusb/gadget/lun0/file
-
-TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=primoc
-BOARD_KERNEL_BASE := 0x14300000
-BOARD_KERNEL_PAGESIZE := 2048
-
-# Wifi
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WLAN_DEVICE_REV            := bcm4330_b1
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcm4330_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcm4330_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcm4330_p2p.bin"
-WIFI_DRIVER_MODULE_NAME          := "dhd"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_WLAN_DEVICE                := bcm4329
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcm4329_p2p.bin"
+WIFI_DRIVER_MODULE_NAME          := "bcm4329"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=wlan"
+BOARD_WLAN_DEVICE_REV            := bcm4329
 WIFI_BAND                        := 802_11_ABG
+CONFIG_P2P := true
 
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
-BOARD_USES_MMCUTILS := false
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
 
-persist.service.adb.enable=1
-ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 
-## TESTING
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88
+BOARD_KERNEL_BASE := 0x04400000
+BOARD_KERNEL_PAGE_SIZE := 4096
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_ADRENO_200 := true
 USE_OPENGL_RENDERER := true
+TARGET_USES_GENLOCK := true
 TARGET_USES_C2D_COMPOSITION := false
 TARGET_USES_SF_BYPASS := false
 TARGET_HAVE_BYPASS := false
-TARGET_USES_OVERLAY := true
-TARGET_QCOM_HDMI_OUT := true
+TARGET_QCOM_HDMI_OUT := false
+TARGET_USES_OVERLAY := false
+BOARD_USE_QCOM_PMEM := false
 TARGET_GRALLOC_USES_ASHMEM := false
-TARGET_USES_GENLOCK := true
-TARGET_FORCE_CPU_UPLOAD := true
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_PROVIDES_LIBLIGHTS := true
+TARGET_GRALLOC_USES_PMEM := true
+
+ENABLE_WTF_USE_ACCELERATED_COMPOSITING := true
+
+BOARD_EGL_CFG := device/htc/primoc/egl.cfg
+
+BOARD_PREBUILT_LIBAUDIO := false
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := saga
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
+BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
