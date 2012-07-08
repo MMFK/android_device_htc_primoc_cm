0d5205b6d2c7b934dfdbd85f6348c196a5b51389
Jmz <jmztaylor@gmail.com>
2012-07-08 12:43:28 -0400

    fix typo

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -45,9 +45,6 @@ LOCAL_SRC_FILES += AudioHardware.cpp
 LOCAL_CFLAGS += -fno-short-enums
 
 LOCAL_STATIC_LIBRARIES += libaudiointerface
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_SHARED_LIBRARIES += liba2dp libbinder
-endif
 
 include $(BUILD_SHARED_LIBRARY)
 
